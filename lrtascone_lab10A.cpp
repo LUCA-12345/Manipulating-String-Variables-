c6cee284d318a82f0abf132b1d6e0a060fea09ae
Luke-Tascone <99374613+Luke-Tascone@users.noreply.github.com>
2022-04-20 14:55:29 -0400

    Add files via upload

---
+++ lrtascone_lab10A.cpp
@@ -0,0 +1,57 @@
+//Lab10A
+//Programmer : Luke Tascone 
+
+#include <iostream>
+#include <string>
+#include <cstdlib> 
+#include <fstream>
+
+
+using namespace std; 
+
+int main ()
+{
+
+    const int SIZE = 51;
+    string info[SIZE]; 
+
+    string searchName;
+
+
+    ifstream inputFile; 
+    inputFile.open("/Users/luketascone/Downloads/lrtascone_lab10A/phonebook.txt"); 
+
+    if (inputFile)
+    {
+        int count;
+
+        for (count = 0; count < SIZE; count++)
+        {
+            getline(inputFile, info[count]);
+        }
+        
+    }
+
+
+    //cout << "test::" << info[0].length() << endl;
+    
+
+    cout << "Enter a name or partial name to search for: ";
+    cin >> searchName;
+    cout << endl;
+
+    cout << "Here are the results of the search" << endl;
+
+
+    for (int count = 0; count < SIZE; count++)                                     
+    {
+    
+        if ((info[count].find(searchName,0)) != -1)
+        {
+            cout << info[count]<< endl; 
+        }
+        
+    }
+    
+    return 0;
+}
